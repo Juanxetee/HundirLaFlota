7df3c2c9a828c69e65b9227f4e867f2ffcba92e4
MariaRepository <159196913+MariaRepository@users.noreply.github.com>
2024-02-24 15:15:32 +0100

    u
    A

---
+++ reparto.py
@@ -0,0 +1,35 @@
+
+Contexto:
+- Tablero: CLASE JUANCHO
+	- Tablero de los barcos del usuario
+	- Tablero del juego del usurario
+	- Tablero oculto del ordenador
+- Barcos (Usuario/ ordenador) DICCIONARIO CLASE. METER EN VARIABLES . ALFONSO
+	- 4x1
+	- 3x2
+	- 2x3
+	- 1x4
+- id de jugador METER EN CLASE
+- Mensaje de bienvenida e instrucciones del juego ALFONSO
+
+
+Funciones:
+- Crear tablero  MARIA
+- Posicionar barcos ALFONSO
+- Disparo del usuario MARCE
+- Disparo del ordenador MARCE
+- Mostrar tablero MARIA
+
+Main : hacer una funcion que llame a las funciones. Incluye comprobación de final de juego si no hay barcos. While True.
+
+Requisitos:
+- Coordenadas:
+	- Viene dadas por "x", "y"
+
+- Barcos:
+	- No se pueden salir del tablero 
+	- Ni superponerese
+
+- Si acierta, sigue jugando
+	- Usuario
+	- Ordenador
